361d65891015fa90a2d0aef7b062e7977057246b
Stephen Furlani <stephen.furlani@upside.com>
2019-12-20 14:02:22 -0500

    Initial Commit

---
+++ CrashingMenuBar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CrashingMenuBar
+//
+//  Created by Stephen Furlani on 12/20/19.
+//  Copyright © 2019 Stephen Furlani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CrashingMenuBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CrashingMenuBar
+//
+//  Created by Stephen Furlani on 12/20/19.
+//  Copyright © 2019 Stephen Furlani. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
